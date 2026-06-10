7757a8439c05a9157cc559fafdf8090aca72a7c4
Lilian | AI Solutions & Business Systems <wowourwedding@gmail.com>
2026-06-10 22:55:42 +0200

    feat: Subject Register + Meeting Minutes template + CSV import for logs (Zambia v46/v36/v20)

--- classmate-zm/sw.js
+++ classmate-zm/sw.js
@@ -3,7 +3,7 @@
    Important for the Zambian audience: schools with patchy connectivity, expensive data.
 */
 
-const SHELL_CACHE = 'classmate-zm-v19';
+const SHELL_CACHE = 'classmate-zm-v20';
 const SHELL = ['./', './index.html'];
 
 self.addEventListener('install', function(e){
