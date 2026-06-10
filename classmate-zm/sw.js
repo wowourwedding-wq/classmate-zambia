6f5fcd41a67a3586b4cb3c92481b588c67c3fdc9
Lilian | AI Solutions & Business Systems <wowourwedding@gmail.com>
2026-06-10 23:01:58 +0200

    feat: Inventory Class/School/Consolidated tabs + expanded fields (Zambia v47/v37/v21)

--- classmate-zm/sw.js
+++ classmate-zm/sw.js
@@ -3,7 +3,7 @@
    Important for the Zambian audience: schools with patchy connectivity, expensive data.
 */
 
-const SHELL_CACHE = 'classmate-zm-v20';
+const SHELL_CACHE = 'classmate-zm-v21';
 const SHELL = ['./', './index.html'];
 
 self.addEventListener('install', function(e){
